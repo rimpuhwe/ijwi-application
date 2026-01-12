de139a64a9b692109d366424a3e393770aa81a92
rimpuhwe <rimpuhwemanzi@gmail.com>
2026-01-12 11:54:57 +0200

    deleted files

--- backend/src/routes/public.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllServices, getAllPortfolios } from '../controllers/publicController';
-
-const router = Router();
-
-router.get('/services', getAllServices);
-router.get('/portfolios', getAllPortfolios);
-
-export default router;
